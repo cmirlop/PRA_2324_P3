4366ed2147a25c5ba7828e40a3cd77e09a7bdddd
cmirlop <carlosnds2000@gmail.com>
2023-12-29 18:03:43 +0100

    Acabado el HashTable

--- HashTable.h
+++ HashTable.h
@@ -15,7 +15,7 @@ class HashTable: public Dict<V>{
 		int max;
 		ListLinked<TableEntry<V>>* table;
 		
-		int h(std:string key){
+		int h(std::string key){
 			int acumulador=0;
 			for(int x=0; x<key.length();x++){
 				acumulador+=int(key.at(x));
@@ -26,50 +26,72 @@ class HashTable: public Dict<V>{
 	public:
 		//Metodos heredados
 		void insert(std::string key, V value) override{
-			if(search(key)){
-				throw std::runtime_erro("El elemento ya esta incluido");
+			TableEntry<V> c(key,value);
+			if(table[h(key)].search(key)!=-1){
+				throw std::runtime_error("El elemento ya esta incluido");
 			}else{
-				TableEntry c(key,value)
-				table->insert(h(key));
+				table[h(key)].prepend(c);
 			}
+			n++;
 		}
 		V search(std::string key) override{
-			V aux = table->search(key);
-			if(aux==NULL){
+			
+			V aux = table[h(key)].search(key); 
+			
+			
+
+			if(aux==-1){
 				throw std::runtime_error("No exite");
 			}
-			return aux;
+			TableEntry<V> valor = table[h(key)].get(aux);
+			return valor.value;
 		}
-		V remove(std::string key) override{
-			V aux = table->remove(key);
-			if(aux == NULL){
-				throw std::runtime_error("no existe");
+
+		V remove(std::string key) override {
+			V aux = table[h(key)].search(key); 
+			if(aux==-1){
+				throw std::runtime_error("No exite");
 			}
-			return aux;
+			TableEntry<V> valor = table[h(key)].remove(aux);
+			n--;
+    			return valor.value;
 		}
+
 		int entries() override{
 			return n;
 		}
 
 		//Metodos propios
 		HashTable(int size){
-			table = new ListLinked<TableEntry<V>>;
-			max =  size;
-			n = 0;
+			max = size;
+			n=0;
+			//table = new ListLinked<V>;
+			//for(int x=0;x<size;x++){
+				table = new ListLinked<TableEntry<V>>[size];
+			//}
+			
+		
+			
 		}
-		~HasTable(){
+		~HashTable(){
 			max = 0;
 			n = 0;
-			delete table;
+			delete[] table;
 		}
 		int capacity(){
 			return max;
 		}
 		friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th){
-			for(int x = 0; x < n; x++){
-				out << "(" << th.table -> data << ")" << std::endl;
-				th.table = th.table->next; 
+			out << "HashTable( entries: " << th.n << " Capacidad: "<< th.max << ")"<<std::endl;
+			for(int y=0; y< th.max; y++){
+				out << "Modulo: " << y <<"[" <<std::endl;
+				for(int x = 0; x < th.table[y].size(); x++){
+					out << "(" << th.table[y].get(x) << ")" << std::endl;
+				 
+				}
+				out << "]"<< std::endl;
 			}
+			//out << th.table;
 			return out;
 		}
 		V operator[](std::string key){
